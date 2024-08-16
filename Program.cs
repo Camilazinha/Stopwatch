6ad5179989051ec203b37c1c9edc2cd4e8f760ef
Camilazinha <caamiladepaula03@gmail.com>
2024-08-16 15:54:55 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Menu();
+    }
+
+    static void Menu()
+    {
+      Console.Clear();
+
+      Console.WriteLine("s = Segundos");
+      Console.WriteLine("m = Minutos");
+      Console.WriteLine("\n0 = Sair");
+      Console.WriteLine("-------------------");
+      Console.WriteLine("Por favor, insira quanto e como deseja contar:");
+
+      string data = Console.ReadLine().ToLower();
+      char type = char.Parse(data.Substring(data.Length - 1, 1));
+      int time = int.Parse(data.Substring(0, data.Length - 1));
+      int multiplier = 1;
+
+      if (type == 'm')
+      {
+        multiplier = 60;
+      }
+      if (time == 0)
+      {
+        System.Environment.Exit(0);
+      }
+
+      Start(time * multiplier);
+
+    }
+    static void Start(int time)
+    {
+      int currentTime = 0;
+
+      while (currentTime != time)
+      {
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+      }
+
+      Console.Clear();
+      Console.WriteLine("Stopwatch finalizado");
+      Thread.Sleep(2000);
+      Menu();
+    }
+  }
+}
